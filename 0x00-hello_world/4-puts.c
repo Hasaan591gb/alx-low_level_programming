cd2f44276d208107fe3c2efeddf7d85a05c84ad4
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-16 13:28:31 -0800

    print on console

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This prints a string to console using puts()
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
