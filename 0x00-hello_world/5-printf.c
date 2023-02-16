119919c9b2b2137f28e4f7c2eed5fe43481e2a31
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-16 13:44:16 -0800

    print string to console using prinf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints string to console using printf()
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
