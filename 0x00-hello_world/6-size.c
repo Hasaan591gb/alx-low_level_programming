433f0610dd42b4ef7d8b4d24b0d499bc7acc0780
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-16 14:02:37 -0800

    print sizes of data types on console

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print sizes of different variable types on console
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	retrn (0);
+}
