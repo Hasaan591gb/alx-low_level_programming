e38a0c46e176dfc738a2d2f70f84c20684dba348
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-17 12:39:18 -0800

    print alphabets to console except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program
+ *
+ * Description: Print alphabets except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	for (; alphabet < 123; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+			putchar(alphabet);
+	}
+	return (0);
+}
