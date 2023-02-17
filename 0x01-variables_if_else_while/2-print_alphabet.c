eb50cf88407534eee62b063627fb7362584e8f4a
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-17 12:05:32 -0800

    print alphabets to console

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Description: Print alphabets to console
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	for (; alphabet < 123; alphabet++)
+		putchar(alphabet);
+	return (0);
+}
