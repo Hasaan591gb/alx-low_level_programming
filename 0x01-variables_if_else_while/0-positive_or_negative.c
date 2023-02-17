e75505887e80ac275bd28c501350d3374f13881c
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-17 10:41:47 -0800

    print a number to console

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,9 +17,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-	elif(n == 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	elif(n < 0)
+	else if (n < 0)
 		printf("%d is negative", n);
 	return (0);
 
