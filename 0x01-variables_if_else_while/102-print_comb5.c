eb3ea2f4454f66207bec24e8b05221bdd5e3ceb8
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-19 05:28:05 -0800

    print four digits in sets of twos

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
- * Description: Print four digits in pairs on two
- * Return: Always 0 (Success)
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i, j;
+	int c, i, k, j;
 
-	for (i = 0; i < 9; i++)
+	for (c = 48; c <= 57; c++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (i = 48; i <= 57; i++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i < 8 || j < 9)
+			for (k = 48; k <= 57; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				for (j = 48; j <= 57; j++)
+				{
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
