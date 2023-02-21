74c550f022943be81549566ebb1cd5edfc51d162
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-21 09:37:08 -0800

    print all minutes of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,21 +7,21 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = '0'; i <= '2'; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			for (k = '0'; k <= '5'; k++)
+			for (k = 0; k <= 5; k++)
 			{
-				for (l = '0'; l <= '9'; l++)
+				for (l = 0; l <= 9; l++)
 				{
-					_putchar(i);
-					_putchar(j);
+					_putchar(i + '0');
+					_putchar(j + '0');
 					_putchar(':');
-					_putchar(k);
-					_putchar(l);
+					_putchar(k + '0');
+					_putchar(l + '0');
 					_putchar('\n');
-					if (i == '2' && j == '3' && k == '5' && l == '9')
+					if (i == 2 && j == 3 && k == 5 && l == 9)
 						break;
 				}
 			}
