d0ee7385a9e801a19abd192b2cf6c6a899a4209f
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-21 06:28:02 -0800

    print alphabets to console x10 through main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets to console 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
