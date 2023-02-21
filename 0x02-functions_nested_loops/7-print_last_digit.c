32ab60707413b978501bc4e9e44f09e2a8f1bc79
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-21 08:28:42 -0800

    return last digit of parameter

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - returns last digit of parameter
- * @digit: parameter on which computation is done
+ * @num: parameter on which computation is done
  * Return: last digit of parameter
  */
 int print_last_digit(int num)
