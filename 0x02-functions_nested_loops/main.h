0b0e23fde171a8bc7452da7da32d58eabe5b646d
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-21 05:19:37 -0800

    print alphabets to console using a local header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
-#define _PUTCHAR
 
+#define _PUTCHAR
 int _putchar(char c);
 
+#define PRINTSTR
 int printstr(char *string);
 
+#define PRINT_ALPHABET
 void print_alphabet(void);
 
 #endif
