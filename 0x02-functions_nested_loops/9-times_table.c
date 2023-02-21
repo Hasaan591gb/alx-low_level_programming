10b92a34c24964882f870fa921c5f82f982f4ddf
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-21 10:35:21 -0800

    print tables from 0 to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,8 +19,6 @@ void times_table(void)
 			first_digit = multiple / 10;
 			if (first_digit > 0)
 				_putchar(first_digit + '0');
-			else
-				_putchar(' ');
 			_putchar(last_digit + '0');
 			if (j != 9)
 			{
