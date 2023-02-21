30288b0718277c6b8c40540cafb72e6a8bfb847d
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-21 08:34:57 -0800

    return last digit of parameter

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,8 @@ int print_last_digit(int num)
 	int last_digit;
 
 	last_digit = num % 10;
+	if (last_digit < 0)
+		last_digit *= -1;
 	_putchar(last_digit + '0');
 	return (last_digit);
 	return (0);
