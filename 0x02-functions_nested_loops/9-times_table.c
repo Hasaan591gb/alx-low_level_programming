132b09f46d40c2bbf37b9f028ba2a18c0319296b
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-21 10:19:21 -0800

    print tables from 0 to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints table to console
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int multiple, last_digit, first_digit;
+
+			multiple = i * j;
+			last_digit = multiple % 10;
+			first_digit = multiple / 10;
+			if (first_digit > 0)
+				_putchar(first_digit + '0');
+			else if (first_digit == 0)
+				_putchar(' ');
+			_putchar(last_digit + '0');
+			if (j != 9)
+				_putchar(',');
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
