9655293e347974a204201e64f13cca29c1a9ec62
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-21 04:27:47 -0800

    using functions from other files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/* function declaration */
+int _putchar(char c);
+
+/**
+ * uses _putchar to print string of char
+ * returns 0 always (success)
+ */
+int printstr(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		_putchar(string[i]);
+	return (0);
+}
+
+/**
+ * main - Entry point for the program
+ * Description: use _putchar.c and main.h to print _putchar
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "_putchar";
+	printstr(str);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define _PUTCHAR
+
+int _putchar(char c);
+
+#endif
