4562269989577d8183c510de760c4c9c04403b56
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-23 13:51:36 -0800

    print given number to console

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints input integer to console
+ * @n: parameter
+ */
+void print_number(int n)
+{
+	int r;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n /= 10;
+	}
+	while (r > 0)
+	{
+		_putchar((r % 10) + 48);
+		r /= 10;
+	}
+	if (n == 0)
+		_putchar(48);
+}
