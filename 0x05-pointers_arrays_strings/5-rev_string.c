c52a91cf6ac401ca40a5742886eea5ce7bbffd95
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-27 14:03:01 -0800

    reverse string parameter

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - return length of string parameter
+ * @str: string parameter
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int length;
+	int i;
+
+	length = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		length++;
+	return (length);
+}
+
+/**
  * rev_string - reverse string parameter
  * @s: string parameter
  */
