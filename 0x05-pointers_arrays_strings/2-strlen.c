308cbb77bfe0895d50437792efb0770c70fb40f1
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-27 12:58:02 -0800

    return length of parameter string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,15 @@
 /**
  * _strlen - return length of string parameter
  * @s: string parameter
+ * Return: length of string
  */
 int _strlen(char *s)
 {
 	int length;
 	int i;
 
+	length = 0;
 	for (i = 0; s[i] != '\0'; i++)
 		length++;
-	return(length);
+	return (length);
 }
