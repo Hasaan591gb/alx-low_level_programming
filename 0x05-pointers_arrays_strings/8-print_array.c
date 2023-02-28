17d039175b4e881c72596b50a5d9c1c35b533de0
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-27 16:48:49 -0800

    print specified number of array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - print parameter number of array elements
+ * @a: array parameter
+ * @n: integer parameter
+ */
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		if (i != (n - 1))
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
