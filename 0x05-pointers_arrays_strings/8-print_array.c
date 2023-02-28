4db36cc76949160fcd7e536c7ee39cd9f72d6c42
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-27 16:57:33 -0800

    print specified number of array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-		if (i != (n - 1))
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		if (i == (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
 	}
 	_putchar('\n');
 }
