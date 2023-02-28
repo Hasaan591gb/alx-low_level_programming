ccb2cc74372d700087b61ca9fcb40380aecdf73b
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-27 16:15:26 -0800

    print every second char in parameter

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - return length of string parameter
+ * @s: string parameter
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length;
+	int i;
+
+	length = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+	return (length);
+}
+
+/**
  * puts2 - prints every second char of str
  * @str: parameter string
  */
@@ -8,7 +24,7 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0' && i !> _strlen(str); i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
