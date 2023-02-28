af199e1f483839a86f247c42a5c694e09f4d1e91
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-27 16:17:09 -0800

    print every second char in parameter

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -24,7 +24,7 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0' && i !> _strlen(str); i += 2)
+	for (i = 0; (str[i] != '\0)' && (i !> _strlen(str)); i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
