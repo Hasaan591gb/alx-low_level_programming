453b977bb8648c589f31fd0e2db26a4cde91fbd6
Hasaan591gb <ahmad591gb@gmail.com>
2023-02-27 17:30:57 -0800

    copy string into a buffer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ void swap_char(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
