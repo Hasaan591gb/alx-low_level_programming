a09d6b31729ac908db89e66fcd8dc3446dba4890
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-02 11:04:53 -0800

    encrypt parameter string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - uses rot13 to encrypt string parameter
+ * @str: string parameter parameter
+ * Return: modified string
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		while (((str[i] < 123) && (str[i] > 96)) || ((str[i] < 91) && (str[i] > 64)))
+		{
+			if (str[i] > 77 || str[i] > 109)
+				str[i] -= 13;
+			else
+				str[i] += 13;
+			break;
+		}
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
+char *rot13(char *str);
 
 #endif /* MAIN_H */
