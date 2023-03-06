abc6e37e5fe2a0f5c68ff16b380672eaf6ae5576
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-06 13:41:44 -0800

    print a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: 2d array parameter
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][8]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
