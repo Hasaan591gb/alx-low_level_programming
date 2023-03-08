58675ab49f7fdf2aca0c09b2f706678751470f26
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-08 15:16:13 -0800

    check whether a number is a prime number or not

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,7 +9,7 @@ int go(int number, int accumulator);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_helper(int n, int i);
-int _sqrt_recursion(int n)i;
+int _sqrt_recursion(int n);
 int _prime_number_helper(int n, int i);
 int is_prime_number(int n);
 
