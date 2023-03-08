b7d5d4c458c8b9c5acafbad0924809b3d30cde71
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-08 11:51:26 -0800

    print a string to console using recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,11 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return;
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	_putchar('\0');
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
 }
