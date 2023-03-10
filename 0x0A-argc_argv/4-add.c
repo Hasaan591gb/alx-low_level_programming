f280a59bee93286baa3f60831c5809495e2b7ba6
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-10 12:29:33 -0800

    write a program that adds numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point for the program
+ * Description: prints sum of arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, value, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		char *arg = argv[i];
+
+		if (!isdigit(arg[0]))
+		{
+			printf("Error\n");
+			return 1;
+		}
+		value = atoi(arg);
+		sum += value;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
