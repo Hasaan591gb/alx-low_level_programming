cc575882ec1354a6bc84b468df08991d0a3c2e48
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-13 12:50:05 -0700

    write a function to return a pointe to a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - make a 2D array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return:  a pointer to a 2 dimensional array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr_grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr_grid = (int **) malloc(sizeof(int) * height);
+	for (i = 0; i < width; i++)
+		*(ptr_grid + i) = (int *) malloc(sizeof(int) * width);
+
+	if (ptr_grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			*(*(ptr_grid + i) + j) = 0;
+	}
+
+	return (ptr_grid);
+}
