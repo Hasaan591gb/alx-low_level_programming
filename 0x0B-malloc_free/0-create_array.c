c320bbdda87d2eea6e80a7145e8744f85e2f19dc
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-13 07:22:07 -0700

    create an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create an array using malloc()
+ * @size: number of elements in array
+ * @c: char to initialize memory with
+ * Return: initialized array of size size
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr_array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr_array = malloc((size * sizeof(char)) + 1);
+	if (ptr_array == NULL)
+		return (NULL);
+
+	for (i = 0; i <= size; i++)
+		*(ptr_array + i) = c;
+	*(ptr_array + i) = '\0';
+	return(ptr_array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+this shows how to dynamically allocate memory
