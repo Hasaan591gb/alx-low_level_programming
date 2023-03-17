26afbeb366df3e8a2cb2054e3590cdc6f9276706
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-16 17:17:48 -0700

    a program that adds two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,34 +2,38 @@
 #include <stdlib.h>
 
 /**
- * _isdigit - checks if parameter is digit
- * @s: parameter
- * Return: 1 if digit, 0 otherwise
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int _isdigit(char *s)
+int is_digit(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
 	}
 	return (1);
 }
 
 /**
- * _strlen - measure length of string parameter
- * @s: string parameter
- * Return: length of string
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int length, i;
+	int i = 0;
 
-	for (i = 0, length = 0; s[i] != '\0'; i++)
-		length++;
-	return (length);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
 
 /**
