4bfd5a22bd8aa9a71c6b961c31190b50a4a7c502
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-20 11:58:02 -0700

    included right header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "dog.h"
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -14,5 +14,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	};
+	}
 }
