f9a43a49620b1e2c1379d3bc17c2c50f5635e8cb
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-20 14:22:50 -0700

    update header file to contain _strlen and _strcpy functions that return length of a string and copy a string into another string respectively

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,7 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 #endif
