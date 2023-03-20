9798b68339bd58c7ec804e8b20baec3093640af2
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-20 13:34:55 -0700

    fixed function call for printf for owner

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,6 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %.6f\n", d->age);
-		prinf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
