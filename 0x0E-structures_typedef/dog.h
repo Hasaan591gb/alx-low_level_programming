0f808f2eaaf098d314ed320ba592bb780c04107e
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-20 11:26:11 -0700

    define new data type called dog that represents a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
