f5d1434ea5920ba890fa6d44f0edfd0bcca0d546
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-20 13:44:50 -0700

    update header file to use struct dog as dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
