da47fc709ccbb41c156deb86e98cffa7f37b23f7
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-20 14:34:49 -0700

    add free_dog that frees memory allocated to d parameter

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
 
 #endif
