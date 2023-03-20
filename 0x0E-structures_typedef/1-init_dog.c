7eb1d88f00996ca96560db6da1661159cf215acd
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-20 11:54:35 -0700

    fixed bug with unnecessary memory allocation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -10,11 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return;
-
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
