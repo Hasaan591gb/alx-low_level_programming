84da142cfcbeaae2df7d98355f07fde919b2aa0f
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-22 15:52:46 -0700

    fix bugs with op dereferencing and spelling for get_op_func()

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,18 +19,18 @@ int main(int argc, char **argv)
 	op = argv[2];
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
+	if (*op != '+' && *op != '-' && *op != '*' && *op != '/' && *op != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (num2 == 0 && (op == '/' || op == '%'))
+	if (num2 == 0 && (*op == '/' || *op == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = (get_op_funct(op))(num1, num2);
+	result = (get_op_func(op))(num1, num2);
 	printf("%d\n", result);
 	return (0);
 }
