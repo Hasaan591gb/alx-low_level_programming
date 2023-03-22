fd53a2f14507cef106b15d473ed92b6ef3d92479
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-22 16:19:17 -0700

    fix bug in declaration of function pointer

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
 int main(int argc, char **argv)
 {
 	int result, num1, num2;
-	char (*op_func)(int, int);
+	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
