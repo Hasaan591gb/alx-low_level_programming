4e699039149f563cb5de4e090937ade153c8c10b
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-22 16:14:39 -0700

    fix bug in calling of op_functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,28 +9,28 @@
 int main(int argc, char **argv)
 {
 	int result, num1, num2;
-	char *op;
+	char (*op_func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	op = argv[2];
+	op_func = get_op_func(argv[2]);
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if (*op != '+' && *op != '-' && *op != '*' && *op != '/' && *op != '%')
+	if (!op_func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (num2 == 0 && (*op == '/' || *op == '%'))
+	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = (get_op_func(op))(num1, num2);
+	result = op_func(num1, num2);
 	printf("%d\n", result);
 	return (0);
 }
