d286017badb21335b53de6a8ad3d25e57f6da88c
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-22 15:06:49 -0700

    file contains functions to do operations only

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - adds integer parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: sum of parameters
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts integer parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: difference of parameters
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies integer parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: product of parameters
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides integer parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: quotient of parameters
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - divides integer parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: modulus of parameters
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
