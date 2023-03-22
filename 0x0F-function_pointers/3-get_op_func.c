99e45946b33de7ff800a22adadeb6b82a74af285
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-22 15:07:59 -0700

    file contains function to find which function to use from 3-op_functions.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function
+ *		 to perform the operation
+ * @s: operation to perform
+ * Return: pointer to function that corresponds
+ *	   to operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while(ops[i].op)
+	{
+		if (*ops[i].op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
