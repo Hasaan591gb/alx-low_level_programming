84b263524c24b07a29a1556ab5effebe026fd8bb
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-23 10:22:45 -0700

    function that prints strings in parameters

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings supplied as parameters
+ * @separator: string to print between strings
+ * @n: number of strings passed to the function
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *str;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *)
+		printf("%s", str ? str : "(nil)");
+		if (separator && i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
