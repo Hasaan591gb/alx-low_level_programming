9e3164655f0837891eb6d12b7047a793d4bbb823
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-23 13:00:47 -0700

    added default case in switch case

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,6 +32,9 @@ void print_all(const char * const format, ...)
 					str = "(nil)";
 				printf("%s%s", separator, str);
 				break;
+			default:
+				i++;
+				continue;
 		}
 		separator = ", ";
 		i++;
