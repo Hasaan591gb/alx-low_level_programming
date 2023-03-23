7965af1b8f698c8d6326f4e1b78aed1d962d8332
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-23 12:51:18 -0700

    function that prints its parameters

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints parameters
+ * @format: string containing keychars that specify variable type
+ */
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *str, *separator;
+	va_list args;
+
+	va_start(args, format);
+	separator = "";
+	i = 0;
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, float));
+				break;
+			case 's':
+				str =  va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+		}
+		separator = ", ";
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
