48c246786d8703cd96e0dc3dc773c8fbbd05a4b2
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-22 17:51:28 -0700

    fix bug in ptr declaration

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,7 +11,7 @@
 int main(int argc, char **argv)
 {
 	unsigned int bytes, i;
-	int (*ptr)(int, char **);
+	char *ptr;
 
 	if (argc != 2)
 	{
@@ -25,10 +25,10 @@ int main(int argc, char **argv)
 		exit(2);
 	}
 
-	ptr = main;
+	ptr = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
-		printf("%02x", *(unsigned char *)(ptr + i));
+		printf("%02x", *(ptr + i));
 		if (i != (bytes - 1))
 			printf(" ");
 	}
