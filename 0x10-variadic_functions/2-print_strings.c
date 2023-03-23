65304ad6162ccf66e4ea77bf5fce49f524529d50
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-23 10:29:13 -0700

    bug fix by adding ; after str value assignment

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char *)
+		str = va_arg(args, char *);
 		printf("%s", str ? str : "(nil)");
 		if (separator && i != (n - 1))
 			printf("%s", separator);
