de35bcaa2945bc613c8a8214d2d49fa969b37d15
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-23 08:24:18 -0700

    local header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
