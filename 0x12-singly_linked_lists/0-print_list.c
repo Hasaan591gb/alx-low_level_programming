b31ee3068c94a79654f6ce197f8225394b700750
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-30 15:45:55 -0700

    bug fix in if

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t nodes;
 
-	for (count = 0; h != NULL; count++)
+	nodes = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		nodes++;
 	}
-
-	return (count);
+	return (nodes);
 }
