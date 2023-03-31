ee5f5e4933f69dcb405ee7766781c77a8f3ebada
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-30 18:06:25 -0700

    contains constructor attribute function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * pre_main - function with constructor attribute
+ * Description: this function will be called automatically
+ * 		before main() is executed
+ */
+void __attribute__ ((constructor)) pre_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
