19c43a5038b7feac65bdd87d75759f6b4a18ef3d
Hasaan591gb <ahmad591gb@gmail.com>
2023-04-04 12:57:52 -0700

    bug fix in type definition of test and new_node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *new_node, *test = *head;
+	listint_t *new_node, *test = *head;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
