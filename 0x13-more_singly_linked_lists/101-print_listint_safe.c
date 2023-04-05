212c74cc9228fafb7636027855779d7981e20f8c
Hasaan591gb <ahmad591gb@gmail.com>
2023-04-04 18:11:55 -0700

    changed algorithm

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,31 +9,33 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *slow, *fast;
+	const listint_t **nodes;
+	size_t i;
 
 	if (head == NULL)
 		exit(98);
 
-	slow = head;
-	fast = head;
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
-		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			exit(98);
-		}
-	}
+	nodes = malloc(sizeof(*nodes) * 1024);
+	if (nodes == NULL)
+		exit(98);
 
 	while (head)
 	{
+		for (i = 0; i < count; i++)
+		{
+			if (nodes[i] == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(nodes);
+				exit(98);
+			}
+		}
+
 		printf("[%p] %d\n", (void *)head, head->n);
+		nodes[count++] = head;
 		head = head->next;
-		count++;
 	}
 
+	free(nodes);
 	return (count);
 }
