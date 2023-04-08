68f428c360d87809ae8ed8cd270aeefda0be60dd
Hasaan591gb <ahmad591gb@gmail.com>
2023-04-08 13:50:34 -0700

    changed algorithm now using recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,39 +1,12 @@
 #include "main.h"
 
 /**
- * exponent - calculates 2 raised to the power of power
- * @power: the power to raise 2 to
- *
- * Return: the value of 2 raised to the power of power
- */
-unsigned int exponent(unsigned int power)
-{
-	if (power == 0)
-		return (1);
-	return (exponent(power - 1) * 2);
-}
-
-/**
  * print_binary - prints the binary representation of a number
  * @n: the number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int bits, flag = 0;
-	unsigned long int num;
-
-	bits = sizeof(num) * 8;
-	num = exponent(bits - 1);
-	for (; num; num >>= 1)
-	{
-		if ((n & num) == num)
-		{
-			_putchar('1');
-			flag++;
-		}
-		else if (flag)
-			_putchar('0');
-	}
-	if (!flag)
-		_putchar('0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) ? '1' : '0');
 }
