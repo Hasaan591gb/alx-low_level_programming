6f95e63058c4b67e4466fe4348195cd4b028150b
Hasaan591gb <ahmad591gb@gmail.com>
2023-04-08 10:01:10 -0700

    reduced characters on 20th line from 82 to less than 80

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,8 @@ unsigned int exponent(unsigned int power)
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1 chars
  *
- * Return: the converted number, or 0 if there is one or more chars in the string
+ * Return: the converted number, or 0 if
+ *	   there is one or more chars in the string
  * b that is not 0 or 1 or if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
