6e3b7a4a048387beef4a38056f224721ca33f853
Hasaan591gb <ahmad591gb@gmail.com>
2023-04-10 15:37:40 -0700

    contains function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int length;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_count)
+	{
+		for (length = 0; text_content[length]; length++)
+			continue;
+		if (write(fd, text_count, length) != length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
