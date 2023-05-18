f403d86e1ce72fc4645bad89a9fc34ee92b850dd
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-18 07:48:34 -0700

    c function that returns node at given index of linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list
+ * @head: Pointer to the head of the list
+ * @index: The index of the node, starting from 0
+ *
+ * Return: The nth node of a dlistint_t linked list, or NULL if it does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	for (; head != NULL; i++)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+	}
+
+	return (NULL);
+}
