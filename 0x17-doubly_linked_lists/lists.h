3b937b7f365aa20e2e57c779694f7757abe8b2dc
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-18 09:15:30 -0700

    included new function prototypes

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -30,6 +30,7 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *alloc_mem(int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
