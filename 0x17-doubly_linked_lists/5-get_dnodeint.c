ed71d8844392377fc16c58f4dfc4a9d5651d6335
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-18 07:50:12 -0700

    made code betty-complaint

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
  * @head: Pointer to the head of the list
  * @index: The index of the node, starting from 0
  *
- * Return: The nth node of a dlistint_t linked list, or NULL if it does not exist
+ * Return: The nth node of a linked list, or NULL if it does not exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
