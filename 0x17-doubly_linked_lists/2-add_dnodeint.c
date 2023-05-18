cd0703c43e3ca8a747a5cfe2102520cae01bd015
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-18 06:53:40 -0700

    bug ix in adding first node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,10 +16,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	temp->prev = NULL;
 	temp->n = n;
+	temp->next = NULL;
 
-	temp->next = *head;
-	(*head)->prev = temp;
-	*head = temp;
+	if (*head == NULL)
+		*head = temp;
+	else
+	{
+		temp->next = *head;
+		(*head)->prev = temp;
+		*head = temp;
+	}
 
 	return (temp);
 }
