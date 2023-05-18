12a10ead9e889972583c7c4d69446da7c4390785
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-18 11:15:53 -0700

    pyscript used to find the palindrome for 102-result

---
+++ 0x17-doubly_linked_lists/is_palindrome.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
+max_palindrome = 0
+for i in range(999, 899, -1):
+    for j in range(999, 899, -1):
+        product = i * j
+        if is_palindrome(product) and product > max_palindrome:
+            max_palindrome = product
+
+print(max_palindrome)
