970e8c96e197bdbbc5cabe95d73561183fb66a5f
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-22 10:48:47 -0700

    file containing functions used in making 100-operations.so

---
+++ 0x18-dynamic_libraries/local_calc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int add(int x, int y)
+{
+	return ((x) + (y));
+}
+
+int sub(int x,  int y)
+{
+	return ((x) - (y));
+}
+
+int mul(int x,  int y)
+{
+	return ((x) * (y));
+}
+
+int div(int x,  int y)
+{
+	return ((x) / (y));
+}
+
+int mod(int x,  int y)
+{
+	return ((x) % (y));
+}
