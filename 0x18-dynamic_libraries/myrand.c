d3f4b695ab072092e73b07cd56423ed2cdc06874
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-22 12:13:44 -0700

    c file used to create mylib.so

---
+++ 0x18-dynamic_libraries/myrand.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int rand(void)
+{
+	static int i = 0;
+
+	i++;
+	if (i == 1)
+		return 8;
+	if (i == 2)
+		return 7;
+	if (i == 3)
+		return 9;
+	if (i == 4)
+		return 23;
+	if (i == 5)
+		return 74;
+	if (i == 6)
+		return 8;
+	else
+		return (i << 2) % 75;
+}
