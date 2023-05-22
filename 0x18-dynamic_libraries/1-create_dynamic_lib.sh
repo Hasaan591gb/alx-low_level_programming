4ee3b68fa12fa5ec0937aa6b41fa9f182bdc1c74
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-22 10:08:58 -0700

    removed -std=gnu89 flag

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -fPIC *.c
+gcc -Wall -pedantic -Werror -Wextra  -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
