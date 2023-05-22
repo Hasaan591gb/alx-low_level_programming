74e6579dc989a8263a0c1d052d106bebd7cdf22d
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-22 10:06:15 -0700

    included strlen

--- 0x18-dynamic_libraries/1-strncat.c
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - return length of string parameter
- * @s: string parameter
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int length;
-	int i;
-
-	length = 0;
-	for (i = 0; s[i] != '\0'; i++)
-		length++;
-	return (length);
-}
-
-/**
  * _strncat - concatenate two strings at most n bytes
  * @dest: parameter to concatenate in
  * @src: parameter to concatenate
